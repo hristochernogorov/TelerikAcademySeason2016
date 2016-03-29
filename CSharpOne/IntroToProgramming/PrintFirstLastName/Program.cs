e05c7bb63ca2e71235adf8cff6b5e32545393878
Hristo Chernogorov <hristochernogorov@gmail.com>
2016-03-29 23:49:30 +0300

    All Task Finished

---
+++ CSharpOne/IntroToProgramming/PrintFirstLastName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrintFirstLastName
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName = "Hristo";
+            string lastName = "Chernogorov";
+            Console.WriteLine("{0}\r\n{1}",firstName,lastName);
+        }
+    }
+}
