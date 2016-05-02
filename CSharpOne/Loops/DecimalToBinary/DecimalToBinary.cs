8fe789b970b7a735b31e8361c91b75d5cb6d5651
hristochernogorov <hristochernogorov@gmail.com>
2016-05-02 17:53:28 +0300

    Finish task

---
+++ CSharpOne/Loops/DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,19 @@
+﻿//Using loops write a program that converts an integer number to its binary representation.
+//The input is entered as long. The output should be a variable of type string.
+//Do not use the built-in .NET functionality.
+//Input
+//On the only input line you will receive the decimal integer number.
+//Output
+//Output a single string - the representation of the input decimal number in it's binary representation.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int decimalNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(decimalNumber,2));
+    }
+}
+
---
+++ CSharpOne/Loops/NumberOneToN/NumberOneToN.cs
@@ -0,0 +1,21 @@
+﻿//Write a program that enters from the console a positive integer n and prints
+//all the numbers from 1 to N inclusive, on a single line, separated by a whitespace.
+//Input
+//The input will consist of a single line - the number N
+//Output
+//The output should consist of a single line - the numbers from 1 to N, separated by a whitespace
+
+using System;
+
+class NumberOneToN
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= number; i++)
+        {
+            Console.Write("{0} ",i);
+        }
+    }
+}
+
