ee1d8e96f831aac453b34c5492a0969eafd06f22
hristochernogorov <hristochernogorov@gmail.com>
2016-05-02 10:17:27 +0300

    All task finished

---
+++ CSharpOne/ConsoleIO/Circle/Circle.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that reads from the console the radius r of a circle and prints its perimeter and area,
+//rounded and formatted with 2 digits after the decimal point.
+//Input
+//On the only line of the input you will receive the radius of the circle - r
+//Output
+//You should print one line only: the perimeter and the area of the circle,
+//separated by a whitespace, and with 2 digits precision
+
+using System;
+
+class Circle
+{
+    static void Main()
+    {
+        double circleRadius = double.Parse(Console.ReadLine());
+        double area = Math.PI * (circleRadius * circleRadius);
+        double perimeter = (2 * Math.PI) * circleRadius;
+        Console.WriteLine("{0:F2} {1:F2}",perimeter,area);
+             
+    }
+}
+
---
+++ CSharpOne/ConsoleIO/SumNumbers/SumNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that reads 3 real numbers from the console and prints their sum.
+//Input
+//On the first line, you will receive the number a
+//On the second line, you will receive the number b
+//On the third line, you will receive the number c
+//Output
+//Your output should consist only of a single line - the sum of the three numbers.
+
+using System;
+
+class SumNumbers
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        double sum = a + b + c;
+        Console.WriteLine(sum);
+    }
+}
+
