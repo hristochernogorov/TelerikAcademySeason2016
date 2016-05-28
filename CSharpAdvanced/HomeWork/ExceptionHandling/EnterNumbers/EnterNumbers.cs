d6956a1bd5ac987e643915a4ff649315fe098775
chernogorov <hristochernogorov@gmail.com>
2016-05-28 16:25:44 +0300

    SquareRoot finish for 30/100

---
+++ CSharpAdvanced/HomeWork/ExceptionHandling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class EnterNumbers
+    {
+        static List<int> ReadNumbers(int start, int end)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var listOfNumbers = new List<int>(input);
+            for (int i = 0; i < end; i++)
+            {
+
+            }
+            return listOfNumbers;
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ CSharpAdvanced/HomeWork/ExceptionHandling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿namespace SquareRoot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class SquareRoot
+    {
+
+       public static double squareRoot;
+        static double SquareOfNumber(int number)
+        {
+            squareRoot = Math.Sqrt(number);
+            return squareRoot;
+        }
+        static void Main()
+        {
+           
+            try
+            {
+                var number = int.Parse(Console.ReadLine());
+                SquareOfNumber(number);
+                Console.WriteLine("{0:F3}", squareRoot);
+                Console.WriteLine("Good bye");
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+                Console.WriteLine("Good bye");
+
+            }
+
+        }
+    }
+}
