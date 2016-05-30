12ed063a0e7e4678fa839aa4d0ad9c7ca715f953
chernogorov <hristochernogorov@gmail.com>
2016-05-30 15:56:03 +0300

    All task is finished

--- ProgramingCourses/CSharpAdvanced/HomeWork/ExceptionHandling/EnterNumbers/EnterNumbers.cs
+++ ProgramingCourses/CSharpAdvanced/HomeWork/ExceptionHandling/EnterNumbers/EnterNumbers.cs
@@ -8,19 +8,50 @@
 
     class EnterNumbers
     {
-        static List<int> ReadNumbers(int start, int end)
+        public static int[] numbers = new int[12];
+        public static bool isIncreasing = true;
+        static int[] ReadNumbers()
         {
-            var input = int.Parse(Console.ReadLine());
-            var listOfNumbers = new List<int>(input);
-            for (int i = 0; i < end; i++)
+            numbers[0] = 1;
+            numbers[11] = 100;
+            int smallest = numbers[0];
+            int start = 0;
+            int end = 100;
+
+            for (int i = 1; i < numbers.Length - 1; i++)
             {
 
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                if (smallest < numbers[i] && numbers[i] < end && numbers[i] > start)
+                {
+                    smallest = numbers[i];
+                    isIncreasing = true;
+                    continue;
+                }
+                else if (smallest >= numbers[i])
+                {
+                    isIncreasing = false;
+                    break;
+                }
             }
-            return listOfNumbers;
+
+            return numbers;
         }
 
         static void Main()
         {
+            ReadNumbers();
+
+            if (isIncreasing)
+            {
+                Console.WriteLine(string.Join(" < ", numbers));
+
+            }
+            if (!isIncreasing)
+            {
+                Console.WriteLine("Exception");
+            }
 
         }
     }
