76c73e4c217b644d916f223f90e1fc81481306a2
chernogorov <hristochernogorov@gmail.com>
2016-06-16 23:47:08 +0300

    Star with new homework tasks

---
+++ ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/Point3D.cs
@@ -0,0 +1,28 @@
+﻿namespace Point
+{
+    public struct Point3D
+    {
+        private static readonly Point3D o =
+            new Point3D(){ X = 0, Y = 0, Z = 0 };
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public static Point3D O
+        {
+            get
+            {
+                return o;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2}", this.X, this.Y, this.Z);
+        }
+    }
+}
---
+++ ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/PointMain.cs
@@ -0,0 +1,13 @@
+﻿namespace Point
+{
+    using System;
+
+    public class PointMain
+    {
+        public static void Main()
+        {
+            var point = new Point3D();
+            Console.WriteLine(Point3D.O);
+        }
+    }
+}
