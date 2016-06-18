741ccf16efdfd9b6bdbb9e35ec8a341c7ed678b5
chernogorov <hristochernogorov@gmail.com>
2016-06-18 20:52:13 +0300

    matrix TODO

--- ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/Point3D.cs
+++ ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/Point3D.cs
@@ -1,15 +1,19 @@
-﻿namespace Point
+﻿using System;
+using System.Linq;
+
+namespace Point
 {
     public struct Point3D
     {
         private static readonly Point3D o =
-            new Point3D(){ X = 0, Y = 0, Z = 0 };
+            new Point3D() { X = 0, Y = 0, Z = 0 };
 
-        public double X { get; set; }
-
-        public double Y { get; set; }
-
-        public double Z { get; set; }
+        public Point3D(double x, double y, double z): this()
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
 
         public static Point3D O
         {
@@ -20,9 +24,25 @@
 
         }
 
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
         public override string ToString()
         {
             return string.Format("{0},{1},{2}", this.X, this.Y, this.Z);
+        }
+        public static Point3D Parse(string text)
+        {
+            int openPar = text.IndexOf('(');
+            double[] coord = text
+                .Substring(openPar + 1, text.Length - openPar - 2)
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => double.Parse(x)).ToArray();
+
+            return new Point3D(coord[0], coord[1], coord[2]);
         }
     }
 }
--- ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/PointMain.cs
+++ ProgramingCourses/OOP/HomeWork/DefiningClassesPartTwo/Point/PointMain.cs
@@ -6,8 +6,27 @@
     {
         public static void Main()
         {
-            var point = new Point3D();
+
+            var point = new Point3D(1, 2, 3);
+
+            var dist = CalculateDistance.Calculate(point, Point3D.O);
+            Console.WriteLine(point);
             Console.WriteLine(Point3D.O);
+            Console.WriteLine(dist);
+            var path = new Path();
+            for (int i = 0; i < 11; i++)
+            {
+                path.AddPoint(new Point3D() { X = i, Y = i * 2, Z = i + 3 });
+            }
+
+            string pathStr = "../../Path.txt";
+            PathStorage.SavePath(path, pathStr);
+            var pathFromFile = PathStorage.LoadPath(pathStr);
+
+            foreach (var p in pathFromFile)
+            {
+                Console.WriteLine(p);
+            }
         }
     }
 }
