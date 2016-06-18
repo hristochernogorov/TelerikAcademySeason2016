ab371c50e13605154d3f8b361be9353c06921d9f
chernogorov <hristochernogorov@gmail.com>
2016-06-19 00:38:57 +0300

    first task finish

--- ProgramingCourses/OOP/HomeWork/ExtensionMethodsDelegatesLambdaLINQ/StringBuilder/StringBuilderMain.cs
+++ ProgramingCourses/OOP/HomeWork/ExtensionMethodsDelegatesLambdaLINQ/StringBuilder/StringBuilderMain.cs
@@ -1,11 +1,14 @@
 ﻿namespace StringBuilder
 {
     using System;
+    using System.Text;
 
     public class StringBuilderMain
     {
         public static void Main()
         {
+            var text = new StringBuilder("OneTwoThree");
+            Console.WriteLine(text.MySubstring(0, 3));
         }
     }
 }
