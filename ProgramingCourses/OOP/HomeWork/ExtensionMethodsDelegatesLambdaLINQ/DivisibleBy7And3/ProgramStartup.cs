c982f7954c6bafff1ad7d48aa19985b06969b081
chernogorov <hristochernogorov@gmail.com>
2016-06-26 14:45:24 +0300

    add some methods

--- ProgramingCourses/OOP/HomeWork/ExtensionMethodsDelegatesLambdaLINQ/DivisibleBy7And3/ProgramStartup.cs
+++ ProgramingCourses/OOP/HomeWork/ExtensionMethodsDelegatesLambdaLINQ/DivisibleBy7And3/ProgramStartup.cs
@@ -3,20 +3,21 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class ProgramStartup
     {
         public static void Main()
         {
-            var array = new int[] {53,34,88,9,39,22,55,40,59,64,61,41,40,
-                                   93,52,47,39,46,54,85,24,84,
-                                   13,27,44,79,9,50,71,36};
-        }
-        private static  DivisibleBy7And3()
-        {
-
+            var array = new int[] { 34, 21, 49, 42, 63, 84, 11, 7 };
+            // with lamba extension
+            var result = array.Where(n => n % 7 == 0 && n % 3 == 0);
+            Console.WriteLine(string.Join(", ", result));
+            // with LINQ extension
+            var secondResult =
+                from number in array
+                where number % 3 == 0 && number % 7 == 0
+                select number;
+            Console.WriteLine(string.Join(", ", secondResult));
         }
     }
 }
